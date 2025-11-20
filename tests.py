0790797a49842ec31f4909f6f0aa6fbf1fd47d0c
David Durost <david.durost@gmail.com>
2025-11-19 19:43:51 -0500

    est template accessibility rules without relying on profile signals

--- tests.py
+++ tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
 
-# Create your tests here.
+from django.contrib.auth import get_user_model
+
+from reports.models import ReportTemplate
+
+
+User = get_user_model()
+
+
+class ReportTemplateTests(TestCase):
+    def test_is_accessible_by_checks_membership(self):
+        owner = User.objects.create_user(
+            username="report.owner",
+            password="pass1234",
+            user_type=User.UserType.ADMIN,
+        )
+        collaborator = User.objects.create_user(
+            username="report.viewer",
+            password="pass1234",
+            user_type=User.UserType.ADMIN,
+        )
+        template = ReportTemplate.objects.create(
+            name="Roster Snapshot",
+            created_by=owner,
+        )
+        template.available_to.add(collaborator)
+
+        self.assertTrue(template.is_accessible_by(owner))
+        self.assertTrue(template.is_accessible_by(collaborator))
